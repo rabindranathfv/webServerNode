a749b6889e33083e8ec5065d8fd9e7a3936d25e0
rabindranathfv <rabindranathucv@gmail.com>
2019-06-12 17:30:00 -0400

    response using http server from node with some parameters

--- webserverNode/app.js
+++ webserverNode/app.js
@@ -1,1 +1,13 @@
+const http = require('http');
 const axios = require('axios');
+
+http.createServer((req, resp) => {
+    resp.writeHead(200, { 'Content-Type': 'application/json' });
+    let answ = {
+        nombre: 'rabin',
+        edad: 28,
+        url: req.url
+    };
+    resp.write(answ.nombre);
+    resp.end();
+}).listen(8080);
