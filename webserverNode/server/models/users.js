8d57535187ce74e43b14b592c5bbacccfa6c16e3
rabindranathfv <rabindranathucv@gmail.com>
2019-06-18 17:17:38 -0400

    create users with post method and store into mongodb

---
+++ webserverNode/server/models/users.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'password is mandatory']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    rol: {
+        type: String,
+        default: 'USER_ROLE'
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('user', userSchema);
--- webserverNode/server/routes/users.js
+++ webserverNode/server/routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const UserModel = require('../models/users');
+
 const app = express();
 
 app.get('/users', (req, res) => {
@@ -7,16 +9,32 @@ app.get('/users', (req, res) => {
 
 app.post('/users', (req, res) => {
     let body = req.body;
-    if (body.name === undefined) {
-        res.status(400).json({
-            ok: false,
-            message: 'name is required'
+
+    console.log('***** Body data *****', req.body);
+
+    let user = new UserModel({
+        name: body.name,
+        email: body.email,
+        password: body.password,
+        rol: body.rol
+    });
+
+    console.log('***** User model instance *****', user);
+
+    user.save((err, userDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            msg: 'create users sucessfully',
+            user: userDB
         });
-    } else {
-        res.json({
-            user: body
-        });
-    }
+    });
+
 });
 
 app.put('/users/:id', (req, res) => {
